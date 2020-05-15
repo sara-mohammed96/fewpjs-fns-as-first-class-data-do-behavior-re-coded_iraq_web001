d730e10a229c67c58454e9c58222f0fa3eebcba3
sara altaee <sara.korea96@gmail.com>
2020-05-15 23:09:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  let t=time.split(":");
+  if(t[0]<12){
+    return `good morning`;
+  }
+  if(t[0]>=12 && time<5){
+    return `good afternoon`;
+  }
+  if(t[0]>5){
+    return `good evening`;
+  }
+}
 /* Write your implementation of displayMessage() */
